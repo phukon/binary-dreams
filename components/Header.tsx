c8bd36176354f330216d548424868ccd1dbdd7f8
phukon <hello@rikiphukon.com>
2024-01-16 00:55:00 +0530

    feat: add header

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useState } from "react";
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="container px-4 mx-auto pt-4">
+      <div className="pr-2 w-full">
+        <div className="border border-black bg-black mt-2 ml-2 w-full h-full">
+          <div className="border-2 border-black bg-white mr-2 -ml-2 mb-2 -mt-2 flex px-6 justify-between items-center">
+            <div className="h-[24px] w-[104px] md:h-[28px] md:w-[120px]">
+              <span className="text-lg">
+                binary<b>dreams</b>
+              </span>
+            </div>
+            <nav
+              className={`md:flex fixed md:relative top-24 md:top-0 left-0 right-0 bottom-0 z-10 bg-white overflow-none ${
+                isActive && "hidden"
+              }`}
+            >
+              <ul className="container px-4 md:px-0 mx-auto flex flex-1 flex-col min-h-full md:flex-row md:gap-x-8 md:items-center relative">
+                <li className="my-4 md:my-3 underline font-semibold decoration-neutral-400">
+                  <p className="text-5xl font-bold md:text-base">
+                    <a className="tracking-[-2px] md:tracking-[-1px]">phukon</a>
+                  </p>
+                </li>
+                <li className="my-4 md:my-3 underline font-semibold decoration-neutral-400">
+                  <p className="text-5xl font-bold md:text-base">
+                    <a className="tracking-[-2px] md:tracking-[-1px]">github</a>
+                  </p>
+                </li>
+                <li className="my-3 absolute md:relative bottom-4 md:bottom-0 left-4 md:left-0 right-4 md:right-0">
+                  <p className="text-5xl font-bold md:text-base">
+                    <a className="tracking-[-2px] md:tracking-[-1px]">
+                      <button className="bg-black text-white px-4 py-3 underline font-semibold decoration-neutral-400 text-xl md:text-sm w-full md:w-auto">
+                        Github
+                      </button>
+                    </a>
+                  </p>
+                </li>
+              </ul>
+            </nav>
+
+            <button
+              className="w-8 h-8 my-3 md:hidden"
+              onClick={() => setIsActive(!isActive)}
+            >
+              <div id="open" className={`${!isActive && "hidden"}`}>
+                <div className="bg-black w-full h-[5px] my-1" />
+                <div className="bg-black w-full h-[5px] my-1" />
+                <div className="bg-black w-full h-[5px] my-1" />
+              </div>
+              <div id="close" className={`${isActive && "hidden"}`}>
+                <div className="bg-black w-4/5 h-[5px] my-1 rotate-45 translate-x-[4px] translate-y-[4px]" />
+                <div className="bg-black w-4/5 h-[5px] my-1 -rotate-45 translate-x-[3px] -translate-y-[5px]" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- stories/Header.stories.ts
+++ stories/Header.stories.ts
@@ -1,14 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Header } from './Header';
+import type { Meta, StoryObj } from "@storybook/react";
+import Header from "@/components/Header";
 
 const meta = {
-  title: 'Example/Header',
+  title: "Header",
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+
+  tags: ["autodocs"],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } satisfies Meta<typeof Header>;
 
@@ -17,10 +16,6 @@ type Story = StoryObj<typeof meta>;
 
 export const LoggedIn: Story = {
   args: {
-    user: {
-      name: 'Jane Doe',
-    },
+    isMobile: false,
   },
 };
-
-export const LoggedOut: Story = {};
