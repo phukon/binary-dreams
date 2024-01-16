6cf820e754079a2b8b43349f8cebd374497a01df
phukon <hello@rikiphukon.com>
2024-01-16 15:17:09 +0530

    fix(scrol): bug on header menu

--- app/Play.tsx
+++ app/Play.tsx
@@ -1,11 +1,22 @@
 import Header from "@/components/Header";
 import Section from "@/components/Section";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Play = () => {
   return (
     <div className="flex flex-col items-center">
       <Header />
       <Section />
+
+      <div>
+
+      </div>
     </div>
   );
 };
