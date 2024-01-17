46056d2b1f516f33284624b7dc624c21f5a5c7cf
phukon <hello@rikiphukon.com>
2024-01-17 17:01:35 +0530

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import CaroPlay from "./CaroPlay";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Header />
       <CaroPlay />
+      <Footer/>
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <section className="pt-6 pb-12 items-center text-justify max-w-[1100px] break-words  text-[#666]">
+      <p>
+        Hats off to Ishan for dropping these mind-bending visuals, straight out
+        of the Midjourney magic on his Twitter¹. Those pictures are weaving the
+        future, technology, and that raw human hustle into a visual feast. I was
+        captivated by the pictures and felt compelled to share this experience,
+        the beauty and wonder encapsulated in each image with a wider audience.
+        Feel free to share your thoughts, you can reach me on my Twitter². <br />
+        Context.:{" "}
+        <a style={{ color: "black" }} href="https://twitter.com/radshaan">
+          [1]
+        </a>
+        <a
+          style={{ color: "black" }}
+          href="https://zerodha.tech/blog/scaling-with-common-sense/"
+        >
+          [2]
+        </a>
+      </p>
+    </section>
+  );
+};
+
+export default Footer;
